455f5af52881e2038d0028d520682d05b73ecb13
Pragmatique <Silchevihor@ukr.net>
2019-05-15 23:15:59 +0300

    Add internationalization to my shop, eng and ru languages was added

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,17 +1,48 @@
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
+from django.utils import translation
+from django.utils.translation import gettext as _
+
 from .models import Category, Product
 
 from cart.forms import CartAddProductForm
 
 # Create your views here.
 def product_list(request, category_slug=None):
+
+    # user_language='en'
+    # translation.activate(user_language)
+    # request.session[translation.LANGUAGE_SESSION_KEY]=user_language
+
+    # if translation.LANGUAGE_SESSION_KEY in request.session[translation.LANGUAGE_SESSION_KEY]:
+    #     del request.session[translation.LANGUAGE_SESSION_KEY]
+
     category = None
     categories = Category.objects.all()
+    for categor in categories:
+        categor.name = _(categor.name)
+
     products = Product.objects.filter(available=True)
+    for product in products:
+        product.name = _(product.name)
+        product.description = _(product.description)
+
     if category_slug:
-        category = get_object_or_404(Category, slug=category_slug)
+        # category = get_object_or_404(Category, slug=category_slug)
+        category = get_object_or_404(categories, slug=category_slug)
+        category.name=_(category.name)
+
         products = products.filter(category=category)
+        for product in products:
+            product.name = _(product.name)
+            product.description = _(product.description)
+
+
+
+            #product.category=_(product.category)
+
+
+
     return render(request,
                   'shop/product/list.html',
                   {'category': category,
@@ -23,6 +54,9 @@ def product_detail(request, id, slug):
                                 id=id,
                                 slug=slug,
                                 available=True)
+    product.name = _(product.name)
+    product.description = _(product.description)
+
     cart_product_form = CartAddProductForm()
     return render(request,
                   'shop/product/detail.html',
